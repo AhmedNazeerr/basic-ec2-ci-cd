93f39198f9bcd033c003ffec43065292e716b459
Ahmed Nazeer <97975048+AhmedNazeerr@users.noreply.github.com>
2023-12-04 21:34:10 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write("Hello its Ahmed Nazeer, testing ci/cd"); 
+  res.write("Hello its Ahmed Nazeer, testing ci/cd lines"); 
   res.end(); 
 }).listen(3000); 
